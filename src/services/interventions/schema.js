1a0cf5358d533768d63da94d7257e8f8cf9aa7eb
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-13 19:27:28 +0100

    Update schema.js

--- src/services/interventions/schema.js
+++ src/services/interventions/schema.js
@@ -7,7 +7,7 @@ const interventionSchema=new Schema(
         userId:{type:String,required:true},
         zipcode:{type:Number,required:true},
         interventionRequested:{type:String,required:true},
-        moreInfo:{type:String,required:true},
+        moreInfo:{type:String},
         answers:{type:Array,default:[]}
     },{timestamps:true}
 )
